d71453328a5b8a2f207ac5733462ad72a97f24f2
Hoo Di Heng <Hoodiheng@gmail.com>
2024-02-01 18:29:47 +0800

    Added functionality for retrieving an access token from a Spotify API request

--- src/main/java/org/example/spotifymusicscraper/controller/SpotifyMusicScraperController.java
+++ src/main/java/org/example/spotifymusicscraper/controller/SpotifyMusicScraperController.java
@@ -1,9 +1,53 @@
 package org.example.spotifymusicscraper.controller;
 
-import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
+import org.example.spotifymusicscraper.SpotifyMusicScraperApplication;
+import org.example.spotifymusicscraper.repository.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.example.spotifymusicscraper.model.*;
+
+//Route incoming HTTP requests
 @RestController
 @ResponseBody
 public class SpotifyMusicScraperController {
+    //
+    private final WebClient webClient;
+    private final SongRepository songRepository;
 
+    //Constructor Injection to autowire songRepository bean as a dependency
+    public SpotifyMusicScraperController(SongRepository songRepository) {
+        this.webClient = WebClient.builder().baseUrl("https://accounts.spotify.com/api").build();
+        this.songRepository = songRepository;
+    }
+    @GetMapping("/spotify")
+    //Fetches a list of Songs from Spotify API and returns it in the response body
+    public APIAccessToken getPlaylistFromSpotify(@RequestBody(required = false) String playlistURL) {
+        String clientId = "077d57140732448996a7b50fedf3fe8f";
+        String clientSecret = "b3531f89fdba47b691a0a22c0194988c";
+        String authorization = clientId + ":" + clientSecret;
+
+        //Request an access token and store it in a POJO
+        APIAccessToken response = this.webClient.post()
+                .uri("/token")
+                //Add HTTP request headers and body according to the format specified by the Spotify API
+                .headers(httpHeaders -> {
+                    //Encode the client id and client credentials using Base64
+                    httpHeaders.add("Content-Type", "application/x-www-form-urlencoded");
+                    httpHeaders.add("Authorization", ("Basic " + Base64.getEncoder().encodeToString(authorization.getBytes(StandardCharsets.UTF_8))));
+                })
+                .bodyValue("grant_type=client_credentials")
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                //Convert JSON-format access token into a POJO type
+                .bodyToMono(APIAccessToken.class)
+                .block();
+        return response;
+    }
 }
---
+++ src/main/java/org/example/spotifymusicscraper/model/APIAccessToken.java
@@ -0,0 +1,32 @@
+package org.example.spotifymusicscraper.model;
+
+//Plain Old Java Object (POJO) to represent a JSON access token
+public class APIAccessToken {
+    private String access_token;
+    private String token_type;
+    private Integer expires_in;
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+
+    public String getToken_type() {
+        return token_type;
+    }
+
+    public void setToken_type(String token_type) {
+        this.token_type = token_type;
+    }
+
+    public Integer getExpires_in() {
+        return expires_in;
+    }
+
+    public void setExpires_in(Integer expires_in) {
+        this.expires_in = expires_in;
+    }
+}
