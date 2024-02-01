d1408f15fb10b28ea3ff57a97972df62672a43a0
Hoo Di Heng <Hoodiheng@gmail.com>
2024-02-01 12:33:06 +0800

    Created Rest Controller

--- src/main/java/org/example/spotifymusicscraper/controller/SpotifyMusicScraperController.java
+++ src/main/java/org/example/spotifymusicscraper/controller/SpotifyMusicScraperController.java
@@ -1,4 +1,9 @@
 package org.example.spotifymusicscraper.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@ResponseBody
 public class SpotifyMusicScraperController {
+
 }
