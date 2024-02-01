a2933475d3ad6adb64827ad51ce0cfaf82fe808c
Hoo Di Heng <Hoodiheng@gmail.com>
2024-02-01 12:10:51 +0800

    Initial commit

---
+++ src/main/java/org/example/spotifymusicscraper/SpotifyMusicScraperApplication.java
@@ -0,0 +1,13 @@
+package org.example.spotifymusicscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyMusicScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyMusicScraperApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/spotifymusicscraper/controller/SpotifyMusicScraperController.java
@@ -0,0 +1,4 @@
+package org.example.spotifymusicscraper.controller;
+
+public class SpotifyMusicScraperController {
+}
