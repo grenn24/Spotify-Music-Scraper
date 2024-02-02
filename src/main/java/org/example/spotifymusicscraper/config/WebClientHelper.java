c87fbe7ca5c3be556c1c0e8979b60a7775430960
Hoo Di Heng <Hoodiheng@gmail.com>
2024-02-02 16:31:38 +0800

    Designed a new configuration bean WebClientHelper for making API calls and retrieving the response in a specified format

---
+++ src/main/java/org/example/spotifymusicscraper/config/WebClientHelper.java
@@ -0,0 +1,80 @@
+package org.example.spotifymusicscraper.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientHelper {
+    public <T> T request(String requestType, HttpHeaders headers, String requestBody, Class<T> responseType, Integer bufferSize, String baseURL, String URI) {
+        //Custom Max In-Memory Size for Buffer Codec to support large JSON responses from APIs
+        WebClient webClient = WebClient.builder().codecs(configurer ->
+                configurer.defaultCodecs().maxInMemorySize(bufferSize * 1024)
+        ).baseUrl(baseURL).build();
+
+        if (requestType.equalsIgnoreCase("get")) {
+            T response = webClient.get()
+                    .uri(URI)
+                    //Add HTTP request headers and body according to the format specified by the Spotify API
+                    .headers(httpHeaders ->
+                            //Encode the client id and client credentials using Base64
+                            httpHeaders.addAll(headers))
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    //Retrieve the JSON response body asynchronously in a POJO type
+                    .bodyToMono(responseType)
+                    .block();
+            return response;
+        }
+
+        if (requestType.equalsIgnoreCase("post")) {
+            T response = webClient.post()
+                    .uri(URI)
+                    //Add HTTP request headers and body according to the format specified by the Spotify API
+                    .headers(httpHeaders ->
+                            //Encode the client id and client credentials using Base64
+                            httpHeaders.addAll(headers))
+                    .bodyValue(requestBody)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    //Retrieve the JSON response body asynchronously in a POJO type
+                    .bodyToMono(responseType)
+                    .block();
+
+            return response;
+        }
+
+        if (requestType.equalsIgnoreCase("put")) {
+            T response = webClient.put()
+                    .uri(URI)
+                    //Add HTTP request headers and body according to the format specified by the Spotify API
+                    .headers(httpHeaders ->
+                            //Encode the client id and client credentials using Base64
+                            httpHeaders.addAll(headers))
+                    .bodyValue(requestBody)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    //Retrieve the JSON response body asynchronously in a POJO type
+                    .bodyToMono(responseType)
+                    .block();
+            return response;
+        }
+
+        if (requestType.equalsIgnoreCase("delete")) {
+            T response = webClient.delete()
+                    .uri(URI)
+                    //Add HTTP request headers and body according to the format specified by the Spotify API
+                    .headers(httpHeaders ->
+                            //Encode the client id and client credentials using Base64
+                            httpHeaders.addAll(headers))
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    //Retrieve the JSON response body asynchronously in a POJO type
+                    .bodyToMono(responseType)
+                    .block();
+            return response;
+        }
+        return null;
+    }
+}
--- src/main/java/org/example/spotifymusicscraper/controller/SpotifyMusicScraperController.java
+++ src/main/java/org/example/spotifymusicscraper/controller/SpotifyMusicScraperController.java
@@ -7,47 +7,36 @@ import java.util.Base64;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.example.spotifymusicscraper.repository.*;
 import org.example.spotifymusicscraper.model.*;
+import org.example.spotifymusicscraper.repository.*;
+import org.example.spotifymusicscraper.config.*;
 
 //Route incoming HTTP requests to different methods
 @RestController
 @ResponseBody
 public class SpotifyMusicScraperController {
-    //
-    private final WebClient webClientForApi;
-    private final WebClient webClientForPlaylist;
     private final SongRepository songRepository;
+    private final WebClientHelper webClientHelper;
 
-    //Using Constructor Injection to autowire songRepository bean as a dependency
-    public SpotifyMusicScraperController(SongRepository songRepository) {
-        this.webClientForApi = WebClient.builder().baseUrl("https://accounts.spotify.com/api").build();
-        //Customise Max In-Memory Size for Buffer Codec to support large JSON responses from APIs
-        this.webClientForPlaylist = WebClient.builder().codecs(configurer ->
-            configurer.defaultCodecs().maxInMemorySize(1024 * 1024)
-        ).baseUrl("https://api.spotify.com").build();
+    @Autowired
+    //Using Constructor Injection to autowire SongRepository and WebClientHelper bean as a dependency
+    public SpotifyMusicScraperController(SongRepository songRepository, WebClientHelper webClientHelper) {
         this.songRepository = songRepository;
+        this.webClientHelper = webClientHelper;
     }
 
-    @GetMapping("/spotify/playlist/{playlistId}")
-    //Fetches a list of Songs from Spotify API and saves it in the database
+    @GetMapping("/spotify/playlist/find/{playlistId}")
+    //Fetches a list of Songs from Spotify API and creates a new song entry in the database table
     public List<Song> getPlaylistFromSpotify(@PathVariable(name="playlistId", required = false) String playlistId) {
-
-        //Fetch a list of songs
-        JSONObject JSONResponse = this.webClientForPlaylist.get()
-                .uri("/v1/playlists/" + playlistId + "/tracks")
-                .header("Authorization", "Bearer " + getAPIAccessToken())
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .bodyToMono(String.class)
-                .map(JSONObject::new)
-                .block();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + getAPIAccessToken());
+        JSONObject JSONSongs = new JSONObject(this.webClientHelper.request("get", headers, "", String.class, 1024, "https://api.spotify.com", "/v1/playlists/" + playlistId + "/tracks"));
 
         //Extract "items" array from original JSON response body
-        List<Song> songs = songParser(JSONResponse);
+        List<Song> songs = songParser(JSONSongs);
         this.songRepository.saveAll(songs);
         return songs;
     }
@@ -58,6 +47,11 @@ public class SpotifyMusicScraperController {
         return this.songRepository.findAll();
     }
 
+    @DeleteMapping("/spotify/playlist/database/reset")
+    public void resetDatabase() {
+        this.songRepository.deleteAll();
+    }
+
     //Retrieving authentication token to attach as header for HTTP requests to Spotify API
     @GetMapping("/spotify")
     public String getAPIAccessToken() {
@@ -65,21 +59,12 @@ public class SpotifyMusicScraperController {
         String clientSecret = "b3531f89fdba47b691a0a22c0194988c";
         String authorization = clientId + ":" + clientSecret;
 
-        //Request an access token and map it to a POJO
-        APIAccessToken accessToken = this.webClientForApi.post()
-                .uri("/token")
-                //Add HTTP request headers and body according to the format specified by the Spotify API
-                .headers(httpHeaders -> {
-                    //Encode the client id and client credentials using Base64
-                    httpHeaders.add("Content-Type", "application/x-www-form-urlencoded");
-                    httpHeaders.add("Authorization", "Basic " + Base64.getEncoder().encodeToString(authorization.getBytes(StandardCharsets.UTF_8)));
-                })
-                .bodyValue("grant_type=client_credentials")
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                //Retrieve the JSON response body asynchronously in a POJO type
-                .bodyToMono(APIAccessToken.class)
-                .block();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/x-www-form-urlencoded");
+        headers.add("Authorization", "Basic " + Base64.getEncoder().encodeToString(authorization.getBytes(StandardCharsets.UTF_8)));
+
+        APIAccessToken accessToken = this.webClientHelper.request("post", headers, "grant_type=client_credentials", APIAccessToken.class, 1024, "https://accounts.spotify.com/api", "/token");
+
         return accessToken.getAccess_token();
     }
 
